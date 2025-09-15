a2db7e3888d06bb963df7856d19caf16c508a06c
Dexter Pressley <103900321+DexterPressley@users.noreply.github.com>
2025-09-15 13:01:02 -0400

    Implemented Search API
    Added contains-style search for First Name, Last Name, Phone, and Email.

---
+++ LAMPAPI/SearchContacts.php
@@ -0,0 +1,81 @@
+
+<?php
+
+	$inData = getRequestInfo();
+
+	$searchEntries = "";
+    $searchSize = 0;
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("SELECT * FROM Contacts WHERE (FirstName LIKE ? OR LastName LIKE ? OR Phone LIKE ? OR Email LIKE ?) AND userID = ?");
+		$searchTerm = "%" . $inData["search"] . "%"; # "Contains"-style search via %.
+        $stmt->bind_param("sssss", $searchTerm, $searchTerm, $searchTerm, $searchTerm, $userId);
+		$stmt->execute();
+		$result = $stmt->get_result();
+
+        if($result->num_rows > 0)
+        {
+            while($row = $result->fetch_assoc())
+            {
+                # Iterative concatenation of search records.
+                if($searchSize > 0){
+                    $searchEntries = sprintf(',%s', $searchEntries);
+                }
+                $searchEntries = sprintf 
+                (
+                    '%s{
+                    "ID": "%d",
+                    "FirstName": "%s",
+                    "LastName": "%s",
+                    "Phone": "%s",
+                    "Email": "%s",
+                    "UserID": "%d"
+                    }',
+                    $searchEntries, $row["ID"], $row["FirstName"], $row["LastName"], $row["Phone"], $row["Email"], $row["UserID"]
+                );
+                $searchSize++;
+            }
+        }
+		if($searchSize != 0)
+		{
+			returnWithInfo($searchEntries);
+		}
+		else
+		{
+			returnWithError("No Records Found");
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"search":"",' . '"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $searchEntries )
+	{
+		$retValue = '{"entries": [' . $searchEntries . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
