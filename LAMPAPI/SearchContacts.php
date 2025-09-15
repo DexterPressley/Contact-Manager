bfaea81aabdd1790166fe19e0578569472c5b594
Dexter Pressley <103900321+DexterPressley@users.noreply.github.com>
2025-09-15 17:34:40 -0400

    Search adjustment

--- LAMPAPI/SearchContacts.php
+++ LAMPAPI/SearchContacts.php
@@ -15,7 +15,7 @@
 	{
 		$stmt = $conn->prepare("SELECT * FROM Contacts WHERE (FirstName LIKE ? OR LastName LIKE ? OR Phone LIKE ? OR Email LIKE ?) AND userID = ?");
 		$searchTerm = "%" . $inData["search"] . "%"; # "Contains"-style search via %.
-        $stmt->bind_param('ss', $searchTerm, $userId);
+        $stmt->bind_param("sssss", $searchTerm, $searchTerm, $searchTerm, $searchTerm, $userId);
 		$stmt->execute();
 		$result = $stmt->get_result();
 
