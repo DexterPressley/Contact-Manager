8cbe7da20b5c27406262743629ff4856737cd42f
Dexter Pressley <103900321+DexterPressley@users.noreply.github.com>
2025-09-16 12:42:58 -0400

    Aligned AddContacts with Integer ID structure.
    Changing JSON I/O to make variables be of their intuitive data type.

--- LAMPAPI/AddContacts.php
+++ LAMPAPI/AddContacts.php
@@ -15,7 +15,7 @@
 	else
 	{
 		$stmt = $conn->prepare("INSERT into Contacts (UserId,FirstName,LastName,Phone,Email) VALUES(?,?,?,?,?)");
-		$stmt->bind_param("sssss", $userId, $firstName, $lastName, $phone, $email);
+		$stmt->bind_param("issss", $userId, $firstName, $lastName, $phone, $email);
 		$stmt->execute();
 		$stmt->close();
 		$conn->close();
